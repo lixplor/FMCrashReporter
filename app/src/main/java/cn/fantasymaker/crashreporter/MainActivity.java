20b06467c81cc1b1a6e9d95f96da22028ce2e71c
fantasymaker <me@fantasymaker.cn>
2016-10-04 10:55:58 +0800

    Change exception demo. Using a button to cause exception instead.

--- app/src/main/java/cn/fantasymaker/crashreporter/MainActivity.java
+++ app/src/main/java/cn/fantasymaker/crashreporter/MainActivity.java
@@ -2,14 +2,18 @@ package cn.fantasymaker.crashreporter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public void onClick(View v) {
         int a = 1 / 0;
     }
 }
